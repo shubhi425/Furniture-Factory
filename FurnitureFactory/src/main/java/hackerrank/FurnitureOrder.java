dac0f0a30cfa40ade26e198ca6fcfc54cc95d1d2
shubhi425 <42046229+shubhi425@users.noreply.github.com>
2021-09-14 13:59:06 +0530

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -6,21 +6,27 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
-
+Private final HashMap<Furniture, Integer> furnitures;
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
+        furnitures= new Hashmap<Furniture, Integer>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+        Integer count=0;
+        if(furnitures.containsKey(type)){
+        count= furnitures.get(type);
+        }
+        furnitures.put(type,count+furnitureCount);
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return new HashMap<furniture,Integer>(furnitures);
     }
 
     public float getTotalOrderCost() {
